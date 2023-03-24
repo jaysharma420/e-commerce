d4e6aa0f6cf32cd01a0c41259c385024f4eeab32
jay123 <jay175147@gmail.com>
2023-03-24 10:55:25 +0530

    first commit

---
+++ e-com/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>E-Com-Dashnoard</h1>
+     <button>Normal Button</button><br/><br/>
+     <Button variant='secondary'>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
